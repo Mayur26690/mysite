f768bae8b23419601c29a1903d24d4ebe67fd971
mayur26690 <Mayur@Niravs-MacBook-Pro.local>
2017-07-13 03:25:19 -0700

    After adding stuuf in model and base templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,9 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 # Create your models here.
+class PublishedManager(models.Manager):
+	"""docstring for PublishedManager"""
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status = 'published')
+
 class Post(models.Model):
 	STATUS_CHOICE = (
 		('draft', 'Draft'),
@@ -20,9 +26,17 @@ class Post(models.Model):
 	updated = models.DateTimeField(auto_now = True)
 	status = models.CharField(max_length = 10, choices = STATUS_CHOICE, default = 'draft')
 
+	objects = models.Manager()
+	published = PublishedManager()
+
 	class meta:
 		ordering = ('-publish')
 
-	
+	def get_absolute_url(self):
+		return reverse('blog:post_details', args = [self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'),self.slug])
+
 	def __str__(self):
 		return self.title	
+
+
+		
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+def post_list(req):
+	posts = Post.published.all()
+	return render(req, 'blog/post/list.html',{'posts':posts})
+
+def post_details(req, year,month,day,post):
+	posst = get_object_or_404(Post, slug = post,status = 'published',
+		publish__year = year, 
+		publish__month = month, 
+		publish__day = day)
+	return render(req, 'blog/post/detail.html',{'post':post})
